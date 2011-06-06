c0515e4a7df5c61f427f6b2db666494190051fed
Craig Fratrik <cfratrik@gmail.com>
2011-06-06 11:30:39 -0700

    better whitespace

--- cp_files.sh
+++ cp_files.sh
@@ -2,17 +2,17 @@
 
 for file in _*
 do
-	path=$(readlink -f $file)
+    path=$(readlink -f $file)
     dotname=.${file:1}
 
-	if [[ -f ~/$dotname ]]
-	then
+    if [[ -f ~/$dotname ]]
+    then
         backup=$dotname.bak
         while [[ -f ~/$backup ]]
         do
             backup=$backup.bak
         done
-		mv ~/$dotname ~/$backup
+        mv ~/$dotname ~/$backup
         echo $backup
     fi
     ln -s $path ~/$dotname
